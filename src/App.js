551cf6df5840d0e6d5f4d5b50b850f330307c607
Suhas Madane <madanesuhas9@gmail.com>
2023-06-15 15:24:41 +0530

    function parameters demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Hello World</h1>
+      <MessageDemo name="Suhas" email="suhasmadane81@gmail.com" />
+    </>
   );
 }
-
 export default App;
+
+function MessageDemo(props) {
+  return (
+    <>
+      <h1>HeLLLo Suhas {props.name}</h1>
+    </>
+  );
+}
