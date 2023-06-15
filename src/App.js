9ff8b476bf39932be79cf28f2a90a9d23521252d
Suhas Madane <madanesuhas9@gmail.com>
2023-06-15 15:59:41 +0530

    normal traverse

--- src/App.js
+++ src/App.js
@@ -2,19 +2,13 @@ function App() {
   return (
     <>
       <h1>Hello World</h1>
-      <MessageDemo name="Suhas" email="suhasmadane81@gmail.com" />
-      <MessageDemo name="Tejas" id="107" />
-      <MessageDemo name="Suraj" email="suraj3@gmail.com" />
-      <MessageDemo name="Vaibhav" id="108" />
+      <ListDemo />
     </>
   );
 }
 export default App;
 //destructring
-function MessageDemo({ email, id }) {
-  return (
-    <>
-      <h1>HeLLLo {id}</h1>
-    </>
-  );
+function ListDemo() {
+  let list = ["Satara", "Navi Mumbai", "Nashik", "Chandrapur"];
+  return <>{list.map((item) => item)};</>;
 }
