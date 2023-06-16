91140d6f15fa7e14ef772f7aa0e3e4e81447ea3c
Suhas Madane <madanesuhas9@gmail.com>
2023-06-17 00:06:30 +0530

    navigation links from another file

--- src/pages/Navigationlinks.js
+++ src/pages/Navigationlinks.js
@@ -1,2 +1,13 @@
-function NavigationLinks() {}
+import { Link } from "react-router-dom";
+
+function NavigationLinks() {
+  return (
+    <>
+      <Link to={"/Home"}>Home</Link>
+      <Link to={"login"}>Login</Link>
+      <Link to={"/register"}>Sign up</Link>
+      <Link to={"/displayUsers"}>See Users</Link>
+    </>
+  );
+}
 export default NavigationLinks;
