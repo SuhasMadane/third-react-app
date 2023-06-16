feea48e1735e95f4b43f4159a9c85c91fba4e894
Suhas Madane <madanesuhas9@gmail.com>
2023-06-17 00:00:54 +0530

    acessing another page from one page with the help of link tag

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import LoginApp from "./pages/login";
 import Register from "./pages/register";
 import DisplayUsers from "./pages/displayUsers";
 import Home from "./pages/Home";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, NavLink, Route, Routes } from "react-router-dom";
 import Login from "./pages/login";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Link to={"/Home"}>Home</Link>
+        <Link to={"login"}>Login</Link>
+        <Link to={"/register"}>Sign up</Link>
+        <Link to={"/displayUsers"}>See Users</Link>
         <Routes>
           <Route path="/Home" element={<Home />} />
           <Route path="/login" element={<Login />} />
